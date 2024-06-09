1c66f97bbb7ddad516ccef576a987d8280ac2308
83546-lavanya-kd2 <lavanya.yadav799@gmail.com>
2024-06-09 23:39:09 +0530

    Classwork Uploaded

---
+++ day02/Election/src/main/java/com/sunbeam/daos/Dao.java
@@ -0,0 +1,21 @@
+package com.sunbeam.daos;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.sunbeam.utils.DbUtils;
+
+public class Dao implements AutoCloseable {
+	protected Connection con;
+	public Dao() throws Exception {
+		con = DbUtils.getConnection();
+	}
+	public void close() {
+		try {
+			if(con != null)
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
